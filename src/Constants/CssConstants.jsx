ea0a1259cc7ab83abe0785dd9f84c392e908dfda
Dhruv_khullar <dhruvkhullar25@gmail.com>
2023-11-27 18:29:10 +1100

    added css constants file

---
+++ src/Constants/CssConstants.jsx
@@ -0,0 +1,18 @@
+export const HEADER_CSS = {
+  mb: '80px',
+  lb_mb: '75px',
+  sm_mb: '35px',
+  padding: '0px 150px',
+  md_padding: '0px 15px',
+  lg_padding: '0px 30px',
+};
+
+export const LOGO_CSS = {
+  width: '175px',
+  fontSize: '25px',
+  fontWeight: 600,
+  md: {
+    width: '145px',
+    fontSize: '20px',
+  },
+};
